0528035d123a739d68a8038dac7ba0b020e096b7
Justin Makaila <justinmakaila@gmail.com>
2014-10-18 00:10:18 -0400

    Added UIKIt import

--- TableAdapter/TableSection.swift
+++ TableAdapter/TableSection.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 public typealias CellConfigurationBlock = (cell: UITableViewCell, item: AnyObject?, indexPath: NSIndexPath) -> ()
 public typealias CellSelectionBlock = (cell: UITableViewCell, indexPath: NSIndexPath) -> ()
